d7fa9a60a76262f4fdc354107f2cf2503f0c460f
aburgy-93 <aburgy@uwalumni.com>
2024-09-10 21:54:34 -0500

    Removed Users tab to prevent sign ups

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -77,12 +77,12 @@ function MainNav() {
             <span>Cabins</span>
           </StyledNavLink>
         </li>
-        <li>
+        {/* <li>
           <StyledNavLink to="/users">
             <HiOutlineUser />
             <span>Users</span>
           </StyledNavLink>
-        </li>
+        </li> */}
         <li>
           <StyledNavLink to="/settings">
             <HiOutlineCog6Tooth />
